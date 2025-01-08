084fd285299b57b207e6c91df49198895748d273
John Wang <john-w@berkeley.edu>
2025-01-08 15:51:15 -0800

    Part 3 done up until sound pads

--- 03. React State/32. Conditional rendering - &&/Joke.jsx
+++ 03. React State/32. Conditional rendering - &&/Joke.jsx
@@ -1,15 +1,23 @@
+import React from "react"
+
 export default function Joke(props) {
     /**
      * Challenge:
      * - Create state `isShown` (boolean, default to `false`)
      * - Add a button that toggles the value back and forth
      */
+    let [isShown, setIsShown] = React.useState(false)
+
+    function toggleShown() {
+        setIsShown((prevIsShown) => !prevIsShown)
+    }
     
     console.log(isShown)
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleShown}>Toggle Punchline</button>
             <hr />
         </div>
     )
--- 03. React State/37. Conditional rendering quiz/quiz.md
+++ 03. React State/37. Conditional rendering quiz/quiz.md
@@ -1,15 +1,16 @@
 1. What is "conditional rendering"?
 
-
+Making a specific component appear/disappear given some sort context.
 
 2. When would you use &&?
 
-
+When you want some simple component show up or not.
 
 3. When would you use a ternary?
 
-
+When you have to decide between two things to display.
 
 4. What if you need to decide between > 2 options on
    what to display?
 
+Do some pre-return processing before rendering the final result.
